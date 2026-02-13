eed0aba45ec523574713dddea0fa21af243ee84e
keegangracey <keegangracey@gmail.com>
2026-02-13 14:53:59 -0500

    ee

--- structural_calcs.py
+++ structural_calcs.py
@@ -11,14 +11,14 @@ def hoop_stress_calcs(r_c_m, t_m, pc_pa, y_s, f, pamb_pa=101325.0):
     # Thin-walled cylinder assumption
     if t_m < r_c_m/10:
         hoop_stress = pc_pa * r_c_m / t_m # Hoop stress (Pa)
-        thrust_stress = -f/(m.pi*((r_c_m + t_m)^2 - r_c_m^2))
+        thrust_stress = -f/(m.pi*((r_c_m + t_m)**2 - r_c_m**2))
     # Thick-walled cylinder assumption (Lame equation)
     elif t_m >= r_c_m/10:
         r_o_m = r_c_m + t_m
         # Finds hoop stress at inner wall (Maximum)
-        thrust_stress = -f/(m.pi*((r_c_m + t_m)^2 - r_c_m^2))
+        thrust_stress = -f/(m.pi*((r_c_m + t_m)**2 - r_c_m**2))
         hoop_stress = (r_c_m**2 * pc_pa - r_o_m**2 * pamb_pa) / (r_o_m**2 - r_c_m**2) - (r_c_m**2 * r_o_m**2 * (pamb_pa - pc_pa)) / (r_c_m**2 * (r_o_m**2 - r_c_m**2))
-    vm_stress = m.sqrt(hoop_stress^2 + thrust_stress^2 - (hoop_stress*thrust_stress))
+    vm_stress = m.sqrt(hoop_stress**2 + thrust_stress**2 - (hoop_stress*thrust_stress))
     fos = y_s/vm_stress    ##It may be max wall, however, hoop_stress is in multiple directions, and thus Von Mises criterion for yielding should be used
 
     print(f"Hoop Stress (Pa): {hoop_stress} Pa")
